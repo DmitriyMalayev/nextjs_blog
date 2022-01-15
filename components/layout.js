7d5441dce96bf73b32e8d62529d5c64ef79d9303
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2022-01-14 23:54:56 -0500

    comments

--- components/layout.js
+++ components/layout.js
@@ -35,7 +35,7 @@ export default function Layout({ children, home }) {
               height={250}
               width={200}
               alt={name}
-              // 936 × 1233
+              // 936 × 1233 (original resolution)
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
